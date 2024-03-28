3e15513a9e695828332a52872345c2929311a0f3
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-03-28 19:06:06 +0530

    updated creating_parquet_schema.py

--- pyspark/dataframe/schema/creating_parquet_schema.py
+++ pyspark/dataframe/schema/creating_parquet_schema.py
@@ -6,25 +6,31 @@ spark = SparkSession.builder \
     .appName("Write Parquet File") \
     .getOrCreate()
 
-# Sample data
-data = [("Basheer", 30, "New York"),
-        ("Krishna", 35, "Los Angeles"),
-        ("Kuldeep", 25, "Chicago")]
+# # Sample data
+# data = [("Basheer", 30, "New York"),
+#         ("Krishna", 35, "Los Angeles"),
+#         ("Kuldeep", 25, "Chicago")]
+#
+# # Define the schema
+# schema = StructType([
+#     StructField("Name", StringType(), nullable=False),
+#     StructField("Age", IntegerType(), nullable=False),
+#     StructField("City", StringType(), nullable=False)
+# ])
+#
+# # Create a DataFrame
+# df = spark.createDataFrame(data, schema)
+#
+# # Path to save the Parquet file
+# parquet_path = "prequet_file.parquet"
+#
+# # Write the DataFrame to Parquet file
+# # df.write.mode("overwrite").parquet("sample_parquet")
+# df.write.format('parquet').mode('overwrite').save('sample_parquet.parquet')
 
-# Define the schema
-schema = StructType([
-    StructField("Name", StringType(), nullable=False),
-    StructField("Age", IntegerType(), nullable=False),
-    StructField("City", StringType(), nullable=False)
-])
+import tempfile
 
-# Create a DataFrame
-df = spark.createDataFrame(data, schema)
+with tempfile.TemporaryDirectory() as d:
+    spark.createDataFrame([{"age": 100, "name": "Hyukjin Kwon"}]).write.mode("overwrite").format("parquet").save(d)
 
-# Path to save the Parquet file
-parquet_path = "pyspark/dataframe/schema/prequet_file.parquet"
-
-# Write the DataFrame to Parquet file
-# df.write.mode("overwrite").parquet("sample_parquet")
-df.write.format('parquet').mode('append').save('sample_parquet')
-
+    spark.read.format('parquet').load(d).show()

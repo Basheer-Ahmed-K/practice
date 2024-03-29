6237a7a48c927f83bc09ce96b71841b5cb00b597
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-03-29 11:51:01 +0530

    updated date_time_functions.py

--- pyspark/dataframe/date_time_functions/date_time_functions.py
+++ pyspark/dataframe/date_time_functions/date_time_functions.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import current_date, current_timestamp
+from pyspark.sql.functions import current_date, current_timestamp, date_add, date_sub, datediff, year, dayofyear, month, \
+        hour, minute, second
 
 spark = SparkSession.builder.appName('DateTime').config("spark.sql.legacy.timeParserPolicy", "LEGACY").getOrCreate()
 
@@ -14,3 +15,24 @@ df.select(current_date()).show()
 
 # displaying current date with time
 df.select(current_timestamp()).show(truncate=False)
+
+# returns a new date by adding a specified number of days to a given start date.
+df.select(date_add("date_col", 3).alias("added 3 days")).show()
+
+# returns a new date by subtracting a specified number of days from a given start date.
+df.select(date_sub("date_col", 5).alias("subtracted 5 days")).show()
+
+# returns the number of days between two dates.
+df.select(datediff("timestamp_col", "date_col").alias("date_diff")).show()
+
+# Extract the year of a given date/timestamp as integer.
+df.select(year("timestamp_col").alias("year")).show()
+
+# Returns the day of the year of the input date
+df.select(dayofyear('date_col').alias("day of year")).show()
+
+# returns the month component of a date.
+df.select(month("date_col").alias("month")).show()
+
+# hour, minute, second - returns corresponding component
+df.select(hour("timestamp_col"), minute("timestamp_col"), second("timestamp_col")).show()

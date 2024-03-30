b68e30861c207664ae6d8ebf8991a5af81e95e29
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-03-30 14:35:59 +0530

    added dense_rank.py

---
+++ pyspark/dataframe/window_functions/dense_rank.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import dense_rank
+from pyspark.sql.window import Window
+
+spark_session = SparkSession.builder.appName('window_function').getOrCreate()
+
+
+emp_data = [(1, 'basheer', 50000, 'IT', 'm'),
+            (2, 'krishna', 60000, 'sales', 'm'),
+            (3, 'kuldeep', 70000, 'marketing', 'm'),
+            (4, 'pratibha', 80000, 'IT', 'm'),
+            (5, 'satiya', 90000, 'sales', 'f'),
+            (6, 'maha', 45000, 'marketing', 'f'),
+            (7, 'kalai', 55000, 'marketing', 'f'),
+            (8, 'amit', 100000, 'IT', 'f'),
+            (9, 'lokesh', 65000, 'IT', 'm'),
+            (10, 'rahul', 50000, 'marketing', 'm'),
+            (11, 'rakhi', 50000, 'IT', 'f'),
+            (12, 'akhilesh', 90000, 'sales', 'm')]
+
+schema = ('id', "name", "salary", "department", "gender")
+
+df = spark_session.createDataFrame(emp_data, schema)
+df.show()
+
+window = Window.partitionBy("department").orderBy("salary")
+
+df.withColumn("dense_rank", dense_rank().over(window)).show()
